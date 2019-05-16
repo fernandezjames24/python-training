798ca6f1c67f9a57fd9184eae9a3b6bc3e642090
fernandezjames24 <50700392+fernandezjames24@users.noreply.github.com>
2019-05-16 19:33:35 +0800

    Create StringFormatting.py

---
+++ PythonBasics/String Formatting/StringFormatting.py
@@ -0,0 +1,15 @@
+
+#codes...
+
+#create a tuple contains "John","Doe", and 53.44
+data = ("John", "Doe", 53.44)
+
+#string format to be used for printing
+format_string = "Hello %s %s. Your current balance is $%.2f."
+
+#printing
+print(format_string % (data[0],data[1],data[2]))
+
+
+
+
