628b4a8956df7f8ccaf46537faeddbe6f93784ea
fernandezjames24 <50700392+fernandezjames24@users.noreply.github.com>
2019-05-16 18:36:49 +0800

    Create Lists.py

---
+++ PythonBasics/Lists/Lists.py
@@ -0,0 +1,30 @@
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+
+#codes....
+
+
+#add numbers 1,2 and 3 to numbers list
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+#add words "hello" and "world" to strings list
+
+strings.append("hello")
+strings.append("world")
+
+#fill in second_name variable with the second name in names variable
+second_name = names[1]
+
+
+#print results
+
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
