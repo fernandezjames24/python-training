7bcfc71c41df4d3859dd5de620413ad8b55539f6
fernandezjames24 <50700392+fernandezjames24@users.noreply.github.com>
2019-05-20 20:50:15 +0800

    Create CodeIntrospection.py

---
+++ PythonAdvanced/Code Introspection/CodeIntrospection.py
@@ -0,0 +1,15 @@
+
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+#instance of Vehicle
+vehicle = Vehicle()
+
+#print all attribute of instance vehicle
+print(dir(vehicle))
